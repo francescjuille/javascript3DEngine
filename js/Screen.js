fde83d5e76d7bd811580e906d6ad5a577dca1d00
Francesc <francescjuille@gmail.com>
2021-05-09 19:08:36 +0200

    first commit

---
+++ js/Screen.js
@@ -0,0 +1,17 @@
+class Screen {
+
+    camera={
+        position:{x:0,y:0,z:0},
+        rotation:{x:0,y:0}
+    }
+
+    objects=[
+    {
+        vertex:{A:{x:0,y:0,z:0},B:{x:0,y:0,z:0},C:{x:0,y:0,z:0},D:{x:0,y:0,z:0},E:{x:0,y:0,z:0},F:{x:0,y:0,z:0},G:{x:0,y:0,z:0},H:{x:0,y:0,z:0}},
+        vertexUnion:["AB","AG","AF","BA","BC","BH","CB","CG","CD","DC","DH","DE","ED","EF","EG","FE","FA","FH","GA","GE","GC","HD","HB","HF"]
+    }
+    ]
+
+
+
+}
